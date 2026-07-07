a280c7315c75597279848aac42be3faded357e4d
areebajabbar942-ajs <areebajabbar942@gmail.com>
2026-07-07 13:27:31 +0500

    client

---
+++ client.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client extends User {
+
+    private String companyName;
+    private String name;
+    private List<Job> jobs;
+    
+    
+
+   public Client(String name, String email, String password, String companyName) {
+
+        super(name,email,password);
+
+        this.companyName = companyName;
+        this.jobs = new ArrayList<>();
+    }
+
+    public Client(String companyName, List<Job> jobs, String name, String email, String password) {
+        super(name, email, password);
+        this.companyName = companyName;
+        this.jobs = jobs;
+    }
+
+    public Job postJob(int jobId, String title, String description, double budget , String deadline) {
+
+        Job job = new Job(jobId, title, description, budget, getName() , deadline);
+
+        jobs.add(job);
+
+        return job;
+    }
+    
+    public String getName(){
+        return super.getname();
+    }
+    
+    public void setName(String name) {
+        this.name = name; // Yeh variable me naya naam daal deta hai
+    }
+
+    public String getJobs() {
+        String result = "";
+        for(Job j : jobs){
+           result = (" " + j + "\n");
+        }
+        return result;
+    }
+    
+    public void setCompanyBio(String companyBio) {
+        this.companyName = companyBio;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+    
+    public List<Job> getJobsList() {
+        return jobs;
+    }
+}
---
+++ job.java
@@ -0,0 +1,55 @@
+public class Job {
+
+    private int jobId;
+    private String title;
+    private String description;
+    private double budget;
+    private String clientName;   
+    private boolean isCompleted;
+    private String deadline;
+
+    public Job(int jobId, String title, String description, double budget, String clientName , String deadline) {
+
+        this.jobId = jobId;
+        this.title = title;
+        this.description = description;
+        this.budget = budget;
+        this.clientName = clientName;
+        this.isCompleted = false;
+        this.deadline = deadline;
+    }
+    
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void markCompleted() {
+        isCompleted = true;
+    }
+
+    public int getJobId() {
+        return jobId;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public boolean isIsCompleted() {
+        return isCompleted;
+    }
+
+     public String getJobInfo() {
+    return jobId + "," +
+           title + "," +
+           description + "," +
+           budget + "," +
+           clientName + "," +
+           deadline + "," +
+           "Open";
+}
+}
